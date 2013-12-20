84c3ce23a9222bb5ce9640383887523f7ca6622b
oxavelar <omar.avelar@gmail.com>
2013-12-19 21:02:33 -0600

    Minor changes.

--- usr/local/etc/Debian Post Install.sh
+++ usr/local/etc/Debian Post Install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install Google Chrome & Google Music.
+# Install Google Chrome & Google Music
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo -e "deb http://dl.google.com/linux/chrome/deb/ stable main
 deb http://dl.google.com/linux/musicmanager/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
@@ -8,11 +8,11 @@ deb http://dl.google.com/linux/musicmanager/deb/ stable main" | sudo tee /etc/ap
 sudo apt-get purge rhythmbox iceweasel ekiga icedtea-* gnome-ppp
 sudo aptitude keep-all
 
-# Installing some common applications I use.
+# Installing some common applications I use
 sudo apt-get update
-sudo apt-get install unrar p7zip wireshark vim xscreensaver-gl-extra vlc banshee pidgin inkscape gimp rsync google-chrome
+sudo apt-get install unrar p7zip wireshark vim xscreensaver-gl-extra vlc banshee pidgin inkscape gimp rsync google-chrome-beta git
 
-# NFS sharing drives.
+# NFS sharing drives
 sudo apt-get install nfs-common nfs-kernel-server
 echo -e "
 /home/omar\t\t10.0.0.0/16(rw,sync,no_subtree_check,crossmnt,no_root_squash)
--- usr/local/etc/Debian Tweaks.sh
+++ usr/local/etc/Debian Tweaks.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+addgroup omar sudo
 sudo echo "Custom tweak script for Debian Linux - by Omar X. Avelar"
 
 # Use the same themes and similar settings
-# for the root user.
+# for the root user
 sudo ln -s ~/.themes /root/.themes
 sudo ln -s ~/.icons /root/.icons
 sudo ln -sf ~/.vimrc /root/.vimrc
@@ -12,21 +13,16 @@ sudo ln -sf ~/.pythonrc /root/.pythonrc
 sudo ln -sf ~/.bashrc /root/.bashrc
 sudo ln -sf ~/.bash_aliases /root/.bash_aliases
 
-# Rebuild fonts cache.
-sudo dpkg-reconfigure fontconfig-config
-sudo dpkg-reconfigure fontconfig
-sudo fc-cache
-
-# Defines root password.
+# Defines root password
 sudo passwd root
 
-# Use less TTY consoles -- maybe 2.
+# Use less TTY consoles -- maybe 2
 sudo vim /etc/default/console-setup
 sudo vim /etc/inittab
 
-# Manual CPU Scaling governor.
+# Manual CPU Scaling governor
 sudo dpkg-reconfigure gnome-applets
 
-# Hibernate file to HDD for sure.
+# Hibernate file to HDD for sure
 #sudo gedit /etc/initramfs-tools/conf.d/resume
 #sudo update-initramfs -u
