40fe50246ad0795985e67d05836095d505460773
oxavelar <omar.avelar@gmail.com>
2015-12-03 06:07:02 -0600

    Portable build.

--- usr/local/etc/libretro-build-unix-x86_64-portable.sh
+++ usr/local/etc/libretro-build-unix-x86_64-portable.sh
@@ -6,7 +6,7 @@
 CURR_DIR=$(realpath ${0%/*})
 LIBRETRO_REPO="https://github.com/libretro/libretro-super"
 LIBRETRO_PATH="$CURR_DIR/$(basename $LIBRETRO_REPO)/"
-OUT_DIR="$CURR_DIR/libretro-portable-out/"
+OUT_DIR="$CURR_DIR/retroarch/"
 
 # Make sure we have libretro super and get inside
 cd $CURR_DIR
@@ -25,7 +25,7 @@ export RARCHCFLAGS="${RARCHCFLAGS} --enable-sse --enable-sse2 --enable-cg --enab
 
 # Build retroarch
 $LIBRETRO_PATH/retroarch-build.sh
-cd $LIBRETRO_PATH/retroarch && make DESTDIR=$OUT_DIR install && cd ..
+cd $LIBRETRO_PATH/retroarch && make DESTDIR="$OUT_DIR/tmp" install && cd ..
 
 # Build libretro cores
 $LIBRETRO_PATH/libretro-fetch.sh
@@ -33,19 +33,22 @@ $LIBRETRO_PATH/libretro-build.sh
 $LIBRETRO_PATH/libretro-install.sh $OUT_DIR
 
 # Organize our files in a portable structure
-mkdir -p "$OUT_DIR/cores-info"
-mkdir -p "$OUT_DIR/cores"
-mkdir -p "$OUT_DIR/shaders"
-mkdir -p "$OUT_DIR/autoconf/"
-find "$OUT_DIR" -iname *.info -exec mv -f \{\} "$OUT_DIR/cores-info/" \;
-find "$OUT_DIR" -iname *.so -exec mv -f \{\} "$OUT_DIR/cores/" \;
-mv -f "$OUT_DIR/usr/local/bin" "$OUT_DIR"
-mv -f "$OUT_DIR/etc" "$OUT_DIR/config"
+mkdir -p "$OUT_DIR/cores-info" "$OUT_DIR/cores" "$OUT_DIR/shaders" "$OUT_DIR/lib" "$OUT_DIR/autoconf/" "$OUT_DIR/downloads/" "$OUT_DIR/system/"  "$OUT_DIR/screenshots/" "$OUT_DIR/assets/" "$OUT_DIR/overlay/" "$OUT_DIR/saves/" "$OUT_DIR/roms/" "$OUT_DIR/remap/" "$OUT_DIR/cheats/"
+find "$OUT_DIR" -iname *.info -exec mv -f \{\} "$OUT_DIR/cores-info/" 2> /dev/null \;
+find "$OUT_DIR" -iname *.so -exec mv -f \{\} "$OUT_DIR/cores/" 2> /dev/null \;
+mv -f "$OUT_DIR/tmp/usr/local/bin" "$OUT_DIR"
+mv -f "$OUT_DIR/tmp/etc" "$OUT_DIR/config"
 mv -f "$OUT_DIR/config/retroarch.cfg" "$OUT_DIR/config/retroarch.cfg.bak"
+# Moving prebuilts
 cp -r "$LIBRETRO_PATH/retroarch/media/shaders_cg" "$OUT_DIR/shaders/"
+cp -r "$LIBRETRO_PATH/retroarch/media/autoconfig/*" "$OUT_DIR/autoconf/joypad"
+cp -r "$LIBRETRO_PATH/retroarch/media/libretrodb/cht/*" "$OUT_DIR/autoconf/cheats"
+
+# Zip for distribution
+zip -r "retroarch-x86_64.zip" "$OUT_DIR"
 
 # Cleanup left-overs
-rm -rf "$OUT_DIR/usr"
+rm -rf "$OUT_DIR/tmp"
 find "$OUT_DIR" -type d -name ".git" -exec rm -rf \{\} \;
 
 sync
