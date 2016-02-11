5f86f44fea2e6e609d80996a9bf286752a3d9e51
oxavelar <omar.avelar@gmail.com>
2016-02-11 07:28:47 -0600

    Adding conversion of shaders.

--- usr/local/etc/libretro-build-unix-x86_64-portable.sh
+++ usr/local/etc/libretro-build-unix-x86_64-portable.sh
@@ -22,8 +22,8 @@ cd $LIBRETRO_PATH/retroarch && git gc && git pull
 
 
 cd $LIBRETRO_PATH
-rm -rf "$OUT_DIR"
-mkdir -p "$OUT_DIR"
+rm -rf $(realpath "$OUT_DIR")
+mkdir -p $(realpath "$OUT_DIR")
 
 # x86_64 optimizations
 export RARCHCFLAGS="${RARCHCFLAGS} --enable-sse --enable-sse2 --enable-cg --enable-libxml2"
@@ -35,7 +35,7 @@ cd $LIBRETRO_PATH/retroarch && make DESTDIR="$OUT_DIR/tmp" install && cd ..
 # Build libretro cores
 $LIBRETRO_PATH/libretro-fetch.sh
 $LIBRETRO_PATH/libretro-build.sh
-$LIBRETRO_PATH/libretro-install.sh $OUT_DIR
+$LIBRETRO_PATH/libretro-install.sh $(realpath $OUT_DIR)
 
 # Organize our files in a portable structure
 mkdir -p "$OUT_DIR/cores-info" "$OUT_DIR/cores" "$OUT_DIR/shaders" "$OUT_DIR/lib" "$OUT_DIR/autoconf/" "$OUT_DIR/downloads/" "$OUT_DIR/system/"  "$OUT_DIR/screenshots/" "$OUT_DIR/assets/" "$OUT_DIR/overlay/" "$OUT_DIR/saves/" "$OUT_DIR/roms/" "$OUT_DIR/remap/" "$OUT_DIR/cheats/"
@@ -49,12 +49,15 @@ cp -r "$LIBRETRO_PATH/retroarch/media/shaders_cg" "$OUT_DIR/shaders/"
 cp -r "$LIBRETRO_PATH/retroarch/media/autoconfig/*" "$OUT_DIR/autoconf/joypad"
 cp -r "$LIBRETRO_PATH/retroarch/media/libretrodb/cht/*" "$OUT_DIR/autoconf/cheats"
 
-# Zip for distribution
-zip -r "retroarch-x86_64.zip" "$OUT_DIR"
-
 # Cleanup left-overs
 rm -rf "$OUT_DIR/tmp"
 find "$OUT_DIR" -type d -name ".git" -exec rm -rf \{\} \;
+
+# Zip for distribution
+zip -r "$OUT_DIR/retroarch-x86_64.zip" "$OUT_DIR"
+
+# Convert shaders
+$LIBRETRO_PATH/retroarch/tools/cg2glsl.py "$LIBRETRO_PATH/retroarch/media/shaders_cg" "$LIBRETRO_PATH/retroarch/media/shaders_glsl"
 
 sync
 
