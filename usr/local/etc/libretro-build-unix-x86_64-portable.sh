bae27f6844bfb80b0c0ebf317f859ac8fdb428b7
oxavelar <omar.avelar@gmail.com>
2016-02-11 23:58:17 -0600

    Conversion path fixes.

--- usr/local/etc/libretro-build-unix-x86_64-portable.sh
+++ usr/local/etc/libretro-build-unix-x86_64-portable.sh
@@ -21,7 +21,7 @@ cd "$LIBRETRO_PATH" && git gc && git pull
 cd $LIBRETRO_PATH/retroarch && git gc && git pull
 
 
-cd $LIBRETRO_PATH
+cd "$LIBRETRO_PATH"
 rm -rf $(realpath "$OUT_DIR")
 mkdir -p $(realpath "$OUT_DIR")
 
@@ -29,21 +29,21 @@ mkdir -p $(realpath "$OUT_DIR")
 export RARCHCFLAGS="${RARCHCFLAGS} --enable-sse --enable-sse2 --enable-cg --enable-libxml2"
 
 # Build retroarch
-$LIBRETRO_PATH/retroarch-build.sh
-cd $LIBRETRO_PATH/retroarch && make DESTDIR="$OUT_DIR/tmp" install && cd ..
+"$LIBRETRO_PATH/retroarch-build.sh"
+cd "$LIBRETRO_PATH/retroarch" && make DESTDIR="$OUT_DIR/tmp" install && cd ..
 
 # Build libretro cores
-$LIBRETRO_PATH/libretro-fetch.sh
-$LIBRETRO_PATH/libretro-build.sh
-$LIBRETRO_PATH/libretro-install.sh $(realpath $OUT_DIR)
+"$LIBRETRO_PATH/libretro-fetch.sh"
+"$LIBRETRO_PATH/libretro-build.sh"
+"$LIBRETRO_PATH/libretro-install.sh" "$OUT_DIR"
 
 # Organize our files in a portable structure
-mkdir -p "$OUT_DIR/cores-info" "$OUT_DIR/cores" "$OUT_DIR/shaders" "$OUT_DIR/lib" "$OUT_DIR/autoconf/" "$OUT_DIR/downloads/" "$OUT_DIR/system/"  "$OUT_DIR/screenshots/" "$OUT_DIR/assets/" "$OUT_DIR/overlay/" "$OUT_DIR/saves/" "$OUT_DIR/roms/" "$OUT_DIR/remap/" "$OUT_DIR/cheats/"
-find "$OUT_DIR" -iname *.info -exec mv -f \{\} "$OUT_DIR/cores-info/" 2> /dev/null \;
-find "$OUT_DIR" -iname *.so -exec mv -f \{\} "$OUT_DIR/cores/" 2> /dev/null \;
-mv -f "$OUT_DIR/tmp/usr/local/bin" "$OUT_DIR"
+mkdir -p "$OUT_DIR/bin" "$OUT_DIR/cores-info" "$OUT_DIR/cores-info" "$OUT_DIR/cores" "$OUT_DIR/shaders" "$OUT_DIR/lib" "$OUT_DIR/autoconf/" "$OUT_DIR/downloads/" "$OUT_DIR/system/" "$OUT_DIR/screenshots/" "$OUT_DIR/assets/" "$OUT_DIR/overlay/" "$OUT_DIR/saves/" "$OUT_DIR/roms/" "$OUT_DIR/remap/" "$OUT_DIR/cheats/"
+mv -f "$OUT_DIR/tmp/usr/local/bin/*" "$OUT_DIR/bin"
 mv -f "$OUT_DIR/tmp/etc" "$OUT_DIR/config"
 mv -f "$OUT_DIR/config/retroarch.cfg" "$OUT_DIR/config/retroarch.cfg.bak"
+find "$OUT_DIR" -iname *.info -exec mv -f \{\} "$OUT_DIR/cores-info/" 2> /dev/null \;
+find "$OUT_DIR" -iname *.so -exec mv -f \{\} "$OUT_DIR/cores/" 2> /dev/null \;
 # Moving prebuilts
 cp -r "$LIBRETRO_PATH/retroarch/media/shaders_cg" "$OUT_DIR/shaders/"
 cp -r "$LIBRETRO_PATH/retroarch/media/autoconfig/*" "$OUT_DIR/autoconf/joypad"
@@ -57,7 +57,8 @@ find "$OUT_DIR" -type d -name ".git" -exec rm -rf \{\} \;
 zip -r "$OUT_DIR/retroarch-x86_64.zip" "$OUT_DIR"
 
 # Convert shaders
-$LIBRETRO_PATH/retroarch/tools/cg2glsl.py "$LIBRETRO_PATH/retroarch/media/shaders_cg" "$LIBRETRO_PATH/retroarch/media/shaders_glsl"
+"$LIBRETRO_PATH/retroarch/tools/cg2glsl.py" "$OUT_DIR/shaders/shaders_cg" "$OUT_DIR/shaders/shaders_glsl"
 
 sync
 
+exit 0
