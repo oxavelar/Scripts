bb8c1a8ba797ec85f078439e2cf3c9d588471e32
oxavelar <omar.avelar@gmail.com>
2016-02-16 23:52:45 -0600

    Polishing some things up.

--- usr/local/etc/libretro-build-unix-x86_64-portable.sh
+++ usr/local/etc/libretro-build-unix-x86_64-portable.sh
@@ -39,25 +39,27 @@ cd "$LIBRETRO_PATH/retroarch" && make DESTDIR="$OUT_DIR/tmp" install && cd ..
 
 # Organize our files in a portable structure
 mkdir -p "$OUT_DIR/bin" "$OUT_DIR/cores-info" "$OUT_DIR/cores-info" "$OUT_DIR/cores" "$OUT_DIR/shaders" "$OUT_DIR/lib" "$OUT_DIR/autoconf/" "$OUT_DIR/downloads/" "$OUT_DIR/system/" "$OUT_DIR/screenshots/" "$OUT_DIR/assets/" "$OUT_DIR/overlay/" "$OUT_DIR/saves/" "$OUT_DIR/roms/" "$OUT_DIR/remap/" "$OUT_DIR/cheats/"
-mv -f "$OUT_DIR/tmp/usr/local/bin/*" "$OUT_DIR/bin"
-mv -f "$OUT_DIR/tmp/etc" "$OUT_DIR/config"
-mv -f "$OUT_DIR/config/retroarch.cfg" "$OUT_DIR/config/retroarch.cfg.bak"
-find "$OUT_DIR" -iname *.info -exec mv -f \{\} "$OUT_DIR/cores-info/" 2> /dev/null \;
-find "$OUT_DIR" -iname *.so -exec mv -f \{\} "$OUT_DIR/cores/" 2> /dev/null \;
+cp -av "$OUT_DIR/tmp/usr/local/bin/." "$OUT_DIR/bin"
+cp -av "$OUT_DIR/tmp/etc/." "$OUT_DIR/config"
+mv -vf "$OUT_DIR/config/retroarch.cfg" "$OUT_DIR/config/retroarch.cfg.bak"
+find "$OUT_DIR" -name "*.info" -exec mv -f \{\} "$OUT_DIR/cores-info/" 2> /dev/null \;
+find "$OUT_DIR" -name "*.so" -exec mv -f \{\} "$OUT_DIR/cores/" 2> /dev/null \;
 # Moving prebuilts
-cp -r "$LIBRETRO_PATH/retroarch/media/shaders_cg" "$OUT_DIR/shaders/"
-cp -r "$LIBRETRO_PATH/retroarch/media/autoconfig/*" "$OUT_DIR/autoconf/joypad"
-cp -r "$LIBRETRO_PATH/retroarch/media/libretrodb/cht/*" "$OUT_DIR/autoconf/cheats"
+cp -av "$LIBRETRO_PATH/retroarch/media/shaders_cg" "$OUT_DIR/shaders"
+cp -av "$LIBRETRO_PATH/retroarch/media/autoconfig" "$OUT_DIR/autoconf/joypad"
+cp -av "$LIBRETRO_PATH/retroarch/media/libretrodb/cht/." "$OUT_DIR/cheats"
 
-# Cleanup left-overs
+# Cleanup left-overs and any .git files for distribution
 rm -rf "$OUT_DIR/tmp"
-find "$OUT_DIR" -type d -name ".git" -exec rm -rf \{\} \;
-
-# Zip for distribution
-zip -r "$OUT_DIR/retroarch-x86_64.zip" "$OUT_DIR"
+( find "$OUT_DIR" -type d -name ".git" \
+  && find . -name ".gitignore" \
+  && find . -name ".gitmodules" ) | xargs rm -rf
 
 # Convert shaders
 "$LIBRETRO_PATH/retroarch/tools/cg2glsl.py" "$OUT_DIR/shaders/shaders_cg" "$OUT_DIR/shaders/shaders_glsl"
+
+# Zip for distribution
+zip -rq "$OUT_DIR/retroarch-x86_64.zip" "$OUT_DIR"
 
 sync
 
